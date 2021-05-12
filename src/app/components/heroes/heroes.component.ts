4cebd7b9679d7bc034e61dac9c91ca9c17f14df4
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-05-12 18:59:58 +0200

    dynamic list display and hero card modifications

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../../interfaces/hero';
+import {HEROES} from '../../services/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+export class HeroesComponent implements OnInit {
+
+  constructor() {
+  }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  ngOnInit(): void {
+
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
